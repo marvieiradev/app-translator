734a62fafbbb9435b107bd4b35f100495f1886b9
Marcos Vieira <marcosvieira482@gmail.com>
2024-02-20 21:37:11 -0300

    feat: add javascript codes

---
+++ script.js
@@ -0,0 +1,80 @@
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTag = document.querySelectorAll("select");
+const icons = document.querySelectorAll(".row i");
+const translateBtn = document.querySelector("button");
+
+//Adicionar o pais com o seu código ao select
+selectTag.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ?
+            "selected" : "" : country_code == "de-DE" ?
+            "selected" : "";
+        let option = `<option ${selected} value="${country_code}">
+        ${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+//Função para trocar os idiomas quando o botão trocar é clicado
+exchangeIcon.addEventListener("click", () => {
+    let tempText = fromText.value;
+    let tempLang = selectTag[0].value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+    if (!fromText.value) {
+        toText.value = "";
+    }
+});
+
+//evento de clique do botão traduzir
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value.trim();
+    let translateFrom = selectTag[0].value;
+    let translateTo = selectTag[1].value;
+    if (!text) return; // se não tem texto, então o imput está vazio
+    toText.setAttribute("placeholder", "Traduzindo...");
+
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        data.matches.forEach(data => {
+            if (data.id === 0) {
+                toText.value = data.translation;
+            }
+        });
+        toText.setAttribute("placeholder", "Tradução");
+    });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({ target }) => {
+        if (!fromText.value || !toText.value) return;
+        //copiar o texto para a área de transferencia
+        if (target.classList.contains("fa-copy")) {
+            if (target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } else {
+            //converter o texto para voz
+            let utterance;
+            if (target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance);
+        }
+    });
+});
+
+
